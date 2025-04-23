7c73e4f038522b849c326b0863bf9ec500759f4d
Andrei Pavlov <andrpav9@gmail.com>
2025-04-23 18:01:26 +0400

    modal transitions

--- src/components/ModalWrapper/ModalWrapper.tsx
+++ src/components/ModalWrapper/ModalWrapper.tsx
@@ -13,17 +13,19 @@ export const ModalWrapper = ({ children }: Props) => {
 
   return (
     <AnimatePresence mode="wait">
-      {isModalOpen && (
-        <motion.div
-          key="modalWrapper"
-          initial={{ opacity: 0, scale: 0.95 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.3 }}
-          className={styles.modalWrapper}
-        >
-          {children}
-        </motion.div>
-      )}
+      <div className={styles.modalWrapper}>
+        {isModalOpen && (
+          <motion.div
+            key="modalWrapper"
+            initial={{ opacity: 0, scale: 0.95 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.3 }}
+            className={styles.modalWrapper}
+          >
+            {children}
+          </motion.div>
+        )}
+      </div>
     </AnimatePresence>
   );
 };
